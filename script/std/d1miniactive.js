6d5f5a89834bfe388c1b147cfeb395ffa613d501
checkerscheid <christian@scheid.email>
2024-04-30 01:37:09 +0200

    D1 Mini Administration
    Create 'Add D1 Mini' with Broadcast, getStatus - Json and save to database with mqtt connection depend on installed sensors

--- script/std/d1miniactive.js
+++ script/std/d1miniactive.js
@@ -9,58 +9,15 @@
 //# Author       : Christian Scheid                                                 #
 //# Date         : 03.04.2024                                                       #
 //#                                                                                 #
-//# Revision     : $Rev:: 585                                                     $ #
+//# Revision     : $Rev:: 593                                                     $ #
 //# Author       : $Author::                                                      $ #
-//# File-ID      : $Id:: d1miniactive.js 585 2024-04-15 22:57:57Z                 $ #
+//# File-ID      : $Id:: d1miniactive.js 593 2024-04-29 17:50:03Z                 $ #
 //#                                                                                 #
 //###################################################################################
 ?> d1miniactive */
 
 p.page.load = function() {
 //###################################################################################
-	$('#globalD1MiniCmd').on('click', '.startSearch', function() {
-		$('.searchResult').addClass('ps-loading').removeClass('ps-container');
-		$('.searchResult .foundNew').text('');
-		$('.searchResult .erg').text('');
-		$.get('std.d1miniactive.startFreakaZoneSearch.req', function(data) {
-			$('.searchResult').removeClass('ps-loading').addClass('ps-container');
-			let foundNew = 0;
-			let htmlNew = `
-<table>
-	<thead>
-		<tr>
-			<th>Name</th>
-			<th>IP</th>
-			<th>MAC</th>
-			<th>Version</th>
-			<th></th>
-		</tr>
-	</thead>
-	<tbody>`;
-			for (const [key, value] of Object.entries(data)) {
-				foundNew++;
-				//mac correction
-				data[key]['Iam']['MAC'] = data[key]['Iam']['MAC'].toLowerCase().replaceAll(':', '');
-				htmlNew += getHtmlNew(key, value.Iam);
-			}
-			htmlNew += `
-	</tbody>
-</table>`;
-			$('.searchResult .foundNew').text(`Neue Devices gefunden: ${foundNew}`);
-			if(foundNew > 0) {
-				$('.searchResult').data('foundNew', data);
-				$('.searchResult .erg').html(htmlNew);
-			}
-			//$('.searchResult pre').text(JSON.stringify(data, null, 4));
-		}, 'json');
-	});
-	$('.searchResult').on('click', '.d1MiniAdd', function() {
-		let myData = $('.searchResult').data('foundNew')[$(this).attr('data-key')]['Iam'];
-		$.post('std.d1miniactive.saveSearchedDevice.req', myData, function(data) {
-			p.page.alert(data);
-		});
-		console.log($('.searchResult').data('foundNew')[$(this).attr('data-key')]['Iam']);
-	});
 	$('.D1MiniDevice').on('click', '.ps-refresh', function() {
 		var tr = $(this).parents('tr:first');
 		var td = $(this).parents('td:first');
@@ -95,7 +52,7 @@ p.page.load = function() {
 };
 
 function D1MiniRenew() {
-	$.get('std.d1miniactive.getsettings.req', function(data) {
+	$.get('std.d1miniactive.getalld1minisettings.req', function(data) {
 		for (const [key, value] of Object.entries(data)) {
 			var mac = value.Mac.toLowerCase().replaceAll(':', '');
 			setTextIfNotStored(key, 'name', value.DeviceName);
@@ -117,15 +74,4 @@ function setTextIfNotStored(name, column, givenValue) {
 		$(td).find('.ps-refresh').removeClass('ps-hidden');
 		$(td).find(`.${column}`).text(givenValue).removeClass('ps-hidden');
 	}
-}
-function getHtmlNew(name, newObj) {
-	let returns = `
-<tr>
-	<td>${newObj.FreakaZoneClient}</td>
-	<td>${newObj.IP}</td>
-	<td>${newObj.MAC}</td>
-	<td>${newObj.Version}</td>
-	<td class="buttonbox"><span class="ps-button d1MiniAdd" data-key="${name}">Add</span></td>
-</tr>`;
-	return returns;
 }
