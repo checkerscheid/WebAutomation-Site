e2dde30e84061767623c33a59b45b007388b4330
Christian Scheid <168387787+checkerscheid@users.noreply.github.com>
2024-06-15 02:54:07 +0200

    new rev

--- script/std/d1minicfg.js
+++ script/std/d1minicfg.js
@@ -9,9 +9,9 @@
 //# Author       : Christian Scheid                                                 #
 //# Date         : 03.04.2024                                                       #
 //#                                                                                 #
-//# Revision     : $Rev:: 619                                                     $ #
+//# Revision     : $Rev:: 622                                                     $ #
 //# Author       : $Author::                                                      $ #
-//# File-ID      : $Id:: d1minicfg.js 619 2024-05-26 02:22:52Z                    $ #
+//# File-ID      : $Id:: d1minicfg.js 622 2024-06-15 00:51:27Z                    $ #
 //#                                                                                 #
 //###################################################################################
 ?> d1minicfg */
