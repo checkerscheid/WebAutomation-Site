2ba9eb3f6ff89d37b2f482fdb47a6353c10b0bce
Christian Scheid <168387787+checkerscheid@users.noreply.github.com>
2024-06-19 21:08:40 +0200

    new rev?

--- script/system/system.js
+++ script/system/system.js
@@ -9,9 +9,9 @@
 //# Author       : Christian Scheid                                                 #
 //# Date         : 06.03.2013                                                       #
 //#                                                                                 #
-//# Revision     : $Rev:: 621                                                     $ #
+//# Revision     : $Rev:: 628                                                     $ #
 //# Author       : $Author::                                                      $ #
-//# File-ID      : $Id:: system.js 621 2024-06-15 00:47:41Z                       $ #
+//# File-ID      : $Id:: system.js 628 2024-06-19 19:08:13Z                       $ #
 //#                                                                                 #
 //###################################################################################
 use system\wpInit;
