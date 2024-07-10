d5a7534c374a39c63004c5c960a7eb8d13d0f57e
Christian Scheid <168387787+checkerscheid@users.noreply.github.com>
2024-07-10 21:45:11 +0200

    publish new settings on D1Mini

--- script/std/d1mini.js
+++ script/std/d1mini.js
@@ -9,9 +9,9 @@
 //# Author       : Christian Scheid                                                 #
 //# Date         : 13.06.2024                                                       #
 //#                                                                                 #
-//# Revision     : $Rev:: 623                                                     $ #
+//# Revision     : $Rev:: 666                                                     $ #
 //# Author       : $Author::                                                      $ #
-//# File-ID      : $Id:: d1mini.js 623 2024-06-15 00:53:46Z                       $ #
+//# File-ID      : $Id:: d1mini.js 666 2024-07-10 19:43:40Z                       $ #
 //#                                                                                 #
 //###################################################################################
 use system\std
@@ -19,11 +19,71 @@ use system\std
 //<? require_once('script/system/websockets.js') ?>
 p.page.load = function() {
 	$.get('std.d1mini.getD1MiniSettings.<?=std::gets("param1")?>.req');
+
+	$('.buttonContainer').on('click', '.SetCmd', function() {
+		var ip = $(this).attr('data-ip');
+		var cmd = $(this).attr('data-cmd');
+		$.post('std.d1mini.setcmd.req', {ip:ip, cmd:cmd});
+	});
+	
 	$('.page').on('click', '.writeTopic', function() {
 		const topic = $(this).attr('data-topic');
 		const value = $(this).attr('data-write');
 		$.post('std.d1mini.writetopic.req', {topic:topic, value:value}, function(data) {
 			
+		});
+	});
+	$('#dialog').on('click', '.writeTopic', function() {
+		const topic = $(this).attr('data-topic');
+		const value = $(this).attr('data-write');
+		const text = $(this).text();
+		$.post('std.d1mini.writetopic.req', {topic:topic, value:value}, function(data) {
+			$('[data-topic="' + topic + '"]').text(text);
+			$('#dialog').dialog('close');
+		});
+	});
+	$('.page').on('click', '.ps-param', function() {
+		const that = $(this)
+		const topic = $(this).attr('data-topic');
+		const unit = $(this).attr('data-unit');
+		$.post('std.write.pop', {headline:topic, unit:unit}, function(data) {
+			$('#dialog').html(data).dialog({
+				title: 'Topic setzen',
+				width: p.popup.width.std,
+				buttons: [{
+					text: 'schreiben',
+					click: function() {
+						const value = $('.popup .newDpVal').val();
+						$.post('std.d1mini.writetopic.req', {topic:topic, value:value}, function(data) {
+							if(data.erg == 'S_OK') {
+								$(that).text(value + ' ' + unit);
+								$('#dialog').dialog('close');
+							} else if(data.erg == 'S_WARNING') {
+								p.page.alertWarning(data.msg);
+							} else if(data.erg == 'S_ERROR') {
+								p.page.alertError(data.msg);
+							}
+						}, 'json');
+					}
+				}, {
+					text: 'Abbrechen',
+					click: function() {
+						$('#dialog').dialog('close');
+					}
+				}]
+			});
+		});
+	});
+	$('.page').on('click', '.ps-parambool', function() {
+		const that = $(this)
+		const topic = $(this).attr('data-topic');
+		const unit = $(this).attr('data-unit');
+		$.post('std.writebool.pop', {topic:topic, unit:unit}, function(data) {
+			$('#dialog').html(data).dialog({
+				title: 'Topic setzen',
+				width: p.popup.width.std,
+				buttons: null
+			});
 		});
 	});
 	ws.connect();
